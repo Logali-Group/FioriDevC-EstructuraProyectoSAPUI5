9de6b508aaf851f06e2324716877683082af060a
Logali-Group <training@logaligroup.com>
2020-03-30 12:37:13 +0000

    Estructura del Proyecto SAPUI5 - Final

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,23 +1,14 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast",
-	"logaligroup/SAPUI5/model/models",
-	"sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast, models, ResourceModel) {
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("logaligroup.SAPUI5.controller.App", {
 
-	/*	onInit: function () {
-			//set model on view
-			this.getView().setModel(models.createRecipient());
+		onInit: function () {
 			
-			// set i18n model on view
-			var i18nModel = new ResourceModel({
-				bundleName: "logaligroup.SAPUI5.i18n.i18n"
-			});
-			this.getView().setModel(i18nModel, "i18n");
-		},*/
+		},
 
 		onShowHello: function () {
 			
